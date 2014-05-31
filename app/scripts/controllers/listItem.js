7dc33b44d01ba3e5dea2ca5afdcdd5bb00d23374
Eric Wooley <ericwooley@gmail.com>
2014-05-31 15:00:08 -0700

    Delete by setting to null instead of splicing, so that indexes are preserved

--- app/scripts/controllers/listItem.js
+++ app/scripts/controllers/listItem.js
@@ -6,12 +6,13 @@ angular.module('App.controllers')
 function ($scope, storage, sp, $ionicPopup, $location, storageKeys) {
 	$scope.lists = storage.get(storageKeys.listsKey);
 	storage.bind($scope,'lists', {defaultValue: {}, storeName: storageKeys.listsKey});
+	console.log($scope.lists[sp.listId].items, sp.itemId);
 	$scope.item = $scope.lists[sp.listId].items[sp.itemId];
 	$scope.listId = sp.listId;
 	$scope.deleteItem= function(){
 		if(confirm('Are you sure you want to delete ' + $scope.lists[sp.listId].items[sp.itemId].name +'?')){
 			// delete $scope.lists[sp.listId].items[sp.itemId]
-			$scope.lists[sp.listId].items.splice(sp.itemId, 1);
+			$scope.lists[sp.listId].items[sp.itemId] = null;
 
 			$location.path('/app/list/'+sp.listId);	
 		}
