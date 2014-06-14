8825b76f9b3c4882cdc566d5687c74ccbecdb194
Eric Wooley <ericwooley@gmail.com>
2014-06-14 12:33:11 -0700

    added more files

--- test/e2e/protractor.conf.js
+++ test/e2e/protractor.conf.js
@@ -1,20 +1,15 @@
 // An example configuration file.
 exports.config = {
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName': 'chrome'
-  },
-
-  // Spec patterns are relative to the current working directly when
-  // protractor is called.
+  capabilities: { 'browserName': 'chrome' },
   specs: [
     'MultipleLists.spec.js',
-    'AddItem.spec.js'
-    ],
-
-  // Options to be passed to Jasmine-node.
+    'AddItem.spec.js',
+    'ItemLocation.spec.js'
+  ],
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 30000,
+    isVerbose: true,
+    // includeStackTrace: false
   }
 };
