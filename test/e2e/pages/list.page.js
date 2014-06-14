52ecdbfd3356b6efb57d0235098d54907e236fe3
Eric Wooley <ericwooley@gmail.com>
2014-06-14 12:32:34 -0700

    reorganize page objects

--- test/e2e/pages/list.page.js
+++ test/e2e/pages/list.page.js
@@ -1,12 +1,25 @@
 'use strict';
 var Page = require('astrolabe').Page;
 
-module.exports = Page.create({
-    newItemNameInput:   { get: function() { return this.findElement(this.by.xpath('/html/body/div[2]/div[2]/input[1]')); } },
-    newItemQtyInput:   { get: function() { return this.findElement(this.by.xpath('/html/body/div[2]/div[2]/input[2]')); } },
+module.exports = Page.create({		
+    addItem: 						{ get: function() { return this.findElement(this.by.id('addItem')); } },
+    newItemNameInput:   { get: function() { return this.findElement(this.by.id('newItemNameInput')); } },
+    newItemQtyInput:   	{ get: function() { return this.findElement(this.by.id('newItemQtyInput')); } },
     addNewItemButton:   { get: function() { return this.findElement(this.by.xpath('/html/body/div[2]/div[3]/button[2]')); } },
-    row1Text:   { get: function() { return this.findElement(this.by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/span/ion-item[1]/span[2]')); } },
-    row1Qty:   { get: function() { return this.findElement(this.by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/span/ion-item[1]/a[1]/span[1]')); } },
-    row2Text:   { get: function() { return this.findElement(this.by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/span/ion-item[2]/span[2]')); } },
-    row2Qty:   { get: function() { return this.findElement(this.by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/span/ion-item[2]/a[1]/span[1]')); } },
+
+		ProduceSelect: 			{ get: function() { return this.findElement(this.by.id('ProduceSelect')); } },
+		FrontOfStoreSelect: { get: function() { return this.findElement(this.by.id('FrontOfStoreSelect')); } },
+		BackWallSelect: 		{ get: function() { return this.findElement(this.by.id('BackWallSelect')); } },
+		BakerySelect: 			{ get: function() { return this.findElement(this.by.id('BakerySelect')); } },
+		MeatAndDeliSelect: 	{ get: function() { return this.findElement(this.by.id('MeatAndDeliSelect')); } },
+		AisleNumInput: 			{ get: function() { return this.findElement(this.by.id('AisleNumInput')); } },
+		saveLocationButton: { get: function() { return this.findElement(this.by.id('saveLocationButton')); } },
+		
+    unknownRow1:   						{ get: function() { return this.findElement(this.by.id('Unknownitem0')); } },
+    unknownRow1Text:   				{ get: function() { return this.findElement(this.by.id('Unknownitem0Name')); } },
+    unknownRow1Qty:   					{ get: function() { return this.findElement(this.by.id('Unknownitem0Qty')); } },
+    unknownItem1EditButton:    { get: function() { return this.findElement(this.by.id('Unknownitem0EditButton')); } },
+    unknownRow2:   						{ get: function() { return this.findElement(this.by.id('Unknownitem1')); } },
+    unknownRow2Text:   				{ get: function() { return this.findElement(this.by.id('Unknownitem1Name')); } },
+    unknownRow2Qty:   					{ get: function() { return this.findElement(this.by.id('Unknownitem1Qty')); } },
 });
--- test/e2e/pages/myLists.page.js
+++ test/e2e/pages/myLists.page.js
@@ -2,5 +2,15 @@
 var Page = require('astrolabe').Page;
 
 module.exports = Page.create({
-    addItem: { get: function() { return this.findElement(this.by.id('addItem')); } }, // finds an input element with the name 'username'
+	addReusableList:{ get: function() { return this.findElement(this.by.id('addResuableList')); } }, 
+	newListNameInput:{ get: function() { return this.findElement(this.by.model('list.name')); } }, 
+	createNewListButton: { get: function() { return this.findElement(this.by.id('createNewList')); } }, 
+  row1EditButton: { get: function() { return this.findElement(this.by.id('list1EditButton')); } },
+  row2EditButton: { get: function() { return this.findElement(this.by.id('list2EditButton')); } },
+  row3EditButton: { get: function() { return this.findElement(this.by.id('list3EditButton')); } },
+  row4EditButton: { get: function() { return this.findElement(this.by.id('list4EditButton')); } },
+  row1Text: { get: function() { return this.findElement(this.by.id('list1Name')); } },
+  row2Text: { get: function() { return this.findElement(this.by.id('list2Name')); } },
+  row3Text: { get: function() { return this.findElement(this.by.id('list3Name')); } },
+  row4Text: { get: function() { return this.findElement(this.by.id('list4Name')); } },
 });
