d9b94d9c38fd02adba609e63d22591c06ff0eba7
Eric Wooley <ericwooley@gmail.com>
2014-06-14 12:33:28 -0700

    use page objects

--- test/e2e/MultipleLists.spec.js
+++ test/e2e/MultipleLists.spec.js
@@ -1,40 +1,38 @@
 'use strict';
+var myListsPage = require('./pages/myLists.page');
 describe('Reusable Lists', function () {
-  beforeEach(function(){
-    browser.driver.get('http://127.0.0.1:9000/#/app/mylists'); 
+  beforeEach(function () {
+    browser.driver.get('http://127.0.0.1:9000/#/app/mylists');
   });
   it('should create list T1', function () {
-    element(by.id('addResuableList')).click();
-    element(by.model('list.name')).sendKeys('Test 1');
-    element(by.id('createNewList')).click();
+    myListsPage.addReusableList.click();
+    myListsPage.newListNameInput.sendKeys('Test 1');
+    myListsPage.createNewListButton.click();
   });
   it('should create list T2', function () {
-    element(by.id('addResuableList')).click();
-    element(by.model('list.name')).sendKeys('Test 2');
-    element(by.id('createNewList')).click();
+    myListsPage.addReusableList.click();
+    myListsPage.newListNameInput.sendKeys('Test 2');
+    myListsPage.createNewListButton.click();
   });
   it('should create list T3', function () {
-    element(by.id('addResuableList')).click();
-    element(by.model('list.name')).sendKeys('Test 3');
-    element(by.id('createNewList')).click();
+    myListsPage.addReusableList.click();
+    myListsPage.newListNameInput.sendKeys('Test 3');
+    myListsPage.createNewListButton.click();
   });
   it('should create list T4', function () {
-    element(by.id('addResuableList')).click();
-    element(by.model('list.name')).sendKeys('Test 4');
-    element(by.id('createNewList')).click();
+    myListsPage.addReusableList.click();
+    myListsPage.newListNameInput.sendKeys('Test 4');
+    myListsPage.createNewListButton.click();
   });
-  it('should have ordered them alphabetically', function(){
+  it('should have ordered them alphabetically', function () {
     var text;
-    text = element(by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/ion-item[3]')).getText();
+    text = myListsPage.row1Text.getText();
     expect(text).toContain('Test 1');
-    
-    text = element(by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/ion-item[4]')).getText();
+    text = myListsPage.row2Text.getText();
     expect(text).toContain('Test 2');
-
-    text = element(by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/ion-item[5]')).getText();
+    text = myListsPage.row3Text.getText();
     expect(text).toContain('Test 3');
-
-    text = element(by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/ion-item[6]')).getText();
-    expect(text).toContain('Test 4'); 
+    text = myListsPage.row4Text.getText();
+    expect(text).toContain('Test 4');
   });
 });
