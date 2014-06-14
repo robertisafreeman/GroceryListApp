68e03c39dca9ffbab7a276935f3690cf0801f22a
Eric Wooley <ericwooley@gmail.com>
2014-06-14 12:31:11 -0700

    added checks that list 2 items aren't in list 1

--- test/e2e/addItem.spec.js
+++ test/e2e/addItem.spec.js
@@ -3,60 +3,83 @@ var listPage = require('./pages/list.page');
 var myListsPage = require('./pages/myLists.page');
 var ptor = protractor.getInstance();
 describe('Add Items', function () {
-  beforeEach(function(){
-    browser.driver.get('http://127.0.0.1:9000/#/app/mylists'); 
+  beforeEach(function () {
+    browser.driver.get('http://127.0.0.1:9000/#/app/mylists');
   });
   describe('to list 1', function () {
     it('Should add 1 Milk to Test 1', function () {
-      element(by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/ion-item[3]/a')).click();
-      myListsPage.addItem.click();
+      myListsPage.row1EditButton.click();
+      listPage.addItem.click();
       listPage.newItemNameInput.sendKeys('Milk');
       listPage.newItemQtyInput.sendKeys('1');
       listPage.addNewItemButton.click();
-      var text = listPage.row1Text.getText();
+      var text = listPage.unknownRow1Text.getText();
       expect(text).toContain('Milk');
-      text = listPage.row1Qty.getText();
+      text = listPage.unknownRow1Qty.getText();
       expect(text).toContain('1');
     });
-    it('should add 2 more milk to the total of milks totaling 3 milks', function(){
-      element(by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/ion-item[3]/a')).click();
-      myListsPage.addItem.click();
+    it('should add 2 more milk to the total of milks totaling 3 milks', function () {
+      myListsPage.row1EditButton.click();
+      listPage.addItem.click();
       listPage.newItemNameInput.sendKeys('Milk');
       listPage.newItemQtyInput.sendKeys('2');
       listPage.addNewItemButton.click();
       ptor.switchTo().alert().accept();
-      var text = listPage.row1Text.getText();
+      var text = listPage.unknownRow1Text.getText();
       expect(text).toContain('Milk');
-
-      text = listPage.row1Qty.getText();
+      text = listPage.unknownRow1Qty.getText();
       expect(text).toContain('3');
-    }); 
+    });
     it('Should add 1 Chip to Test 1', function () {
-      element(by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/ion-item[3]/a')).click();
-      myListsPage.addItem.click();
+      myListsPage.row1EditButton.click();
+      listPage.addItem.click();
       listPage.newItemNameInput.sendKeys('Chip');
       listPage.newItemQtyInput.sendKeys('1');
       listPage.addNewItemButton.click();
-      var text = listPage.row2Text.getText();
+      var text = listPage.unknownRow2Text.getText();
       expect(text).toContain('Chip');
-      browser.debugger();
-      text = listPage.row2Qty.getText();
+      text = listPage.unknownRow2Qty.getText();
       expect(text).toContain('1');
     });
-    it('should add 2 more chips to the total of chips totaling 3 chips', function(){
-      element(by.xpath('/html/body/ion-nav-view/div/ion-pane/ion-nav-view/ion-view/ion-content/div[1]/ion-list/div/ion-item[3]/a')).click();
-      myListsPage.addItem.click();
+    it('should add 2 more chips to the total of chips totaling 3 chips', function () {
+      myListsPage.row1EditButton.click();
+      listPage.addItem.click();
       listPage.newItemNameInput.sendKeys('Chip');
       listPage.newItemQtyInput.sendKeys('2');
       listPage.addNewItemButton.click();
       ptor.switchTo().alert().accept();
-      var text = listPage.row2Text.getText();
+      var text = listPage.unknownRow2Text.getText();
       expect(text).toContain('Chips');
-
-      text = listPage.row2Qty.getText();
+      text = listPage.unknownRow2Qty.getText();
       expect(text).toContain('3');
-      browser.debugger();
-    });  
+    });
   });
-  
+	describe('to list 2', function () {
+		it('should not contain anything from list 1', function () {
+			myListsPage.row2EditButton.click();
+			var pageText = element(by.xpath('/html')).getText();
+			expect(pageText).not.toContain('Milk');
+			expect(pageText).not.toContain('Chip');
+			
+		});
+		it('should add Cheese', function () {
+			myListsPage.row2EditButton.click();
+			listPage.addItem.click();
+      listPage.newItemNameInput.sendKeys('Cheese');
+      listPage.newItemQtyInput.sendKeys('1');
+      listPage.addNewItemButton.click();
+      var text = listPage.unknownRow1Text.getText();
+      expect(text).toContain('Cheese');
+      text = listPage.unknownRow1Qty.getText();
+      expect(text).toContain('1');
+		});
+	});
+	describe('list 1', function () {
+		it('should not contain cheese from list 2', function () {
+			browser.debugger();
+			myListsPage.row1EditButton.click();
+			var pageText = element(by.xpath('/html')).getText();
+			expect(pageText).not.toContain('Cheese');
+		});
+	});
 });
