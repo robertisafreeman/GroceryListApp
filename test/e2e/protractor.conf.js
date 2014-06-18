f263132c4243d21e8de22129f76f8186d9a67716
Eric Wooley <ericwooley@gmail.com>
2014-06-17 21:12:34 -0700

    Switched to use ripple

--- test/e2e/protractor.conf.js
+++ test/e2e/protractor.conf.js
@@ -1,4 +1,6 @@
+'use strict';
 // An example configuration file.
+/* global protractor */
 exports.config = {
   capabilities: { 'browserName': 'chrome' },
   specs: [
@@ -11,5 +13,14 @@ exports.config = {
     defaultTimeoutInterval: 30000,
     isVerbose: true,
     // includeStackTrace: false
-  }
+  },
+  allScriptsTimeout: 20000,
+  onPrepare: function(){
+      var ptor = protractor.getInstance();
+      browser.driver.manage().window().maximize();  
+      // ptor.ignoreSynchronization = true;    
+      browser.driver.get('http://localhost:4400/?enableripple=cordova-3.0.0-iPhone5');
+      browser.sleep(2000);
+      browser.driver.switchTo().frame(0);
+    }
 };
