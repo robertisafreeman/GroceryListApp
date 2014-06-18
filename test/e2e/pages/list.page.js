6a7fd14c776d4b87f109a959a7d15b04d1f0f358
Eric Wooley <ericwooley@gmail.com>
2014-06-17 21:12:55 -0700

    Updated pages

--- test/e2e/pages/list.page.js
+++ test/e2e/pages/list.page.js
@@ -15,11 +15,20 @@ module.exports = Page.create({
 		AisleNumInput: 			{ get: function() { return this.findElement(this.by.id('AisleNumInput')); } },
 		saveLocationButton: { get: function() { return this.findElement(this.by.id('saveLocationButton')); } },
 		
-    unknownRow1:   						{ get: function() { return this.findElement(this.by.id('Unknownitem0')); } },
-    unknownRow1Text:   				{ get: function() { return this.findElement(this.by.id('Unknownitem0Name')); } },
-    unknownRow1Qty:   					{ get: function() { return this.findElement(this.by.id('Unknownitem0Qty')); } },
-    unknownItem1EditButton:    { get: function() { return this.findElement(this.by.id('Unknownitem0EditButton')); } },
-    unknownRow2:   						{ get: function() { return this.findElement(this.by.id('Unknownitem1')); } },
-    unknownRow2Text:   				{ get: function() { return this.findElement(this.by.id('Unknownitem1Name')); } },
-    unknownRow2Qty:   					{ get: function() { return this.findElement(this.by.id('Unknownitem1Qty')); } },
+
+		// Milk
+    milk:   						{ get: function() { return this.findElement(this.by.id('milk-row')); } },
+    milkText:   				{ get: function() { return this.findElement(this.by.id('milk-text')); } },
+    milkQty:   					{ get: function() { return this.findElement(this.by.id('milk-qty')); } },
+    milkEditButton:    { get: function() { return this.findElement(this.by.id('milk-EditButton')); } },
+    // Chips
+    chip:   						{ get: function() { return this.findElement(this.by.id('chip-row')); } },
+    chipText:   				{ get: function() { return this.findElement(this.by.id('chip-text')); } },
+    chipQty:   					{ get: function() { return this.findElement(this.by.id('chip-qty')); } },
+    chipQtyType:   					{ get: function() { return this.findElement(this.by.id('chip-qtyType')); } },
+    // Cheese
+    cheese:   						{ get: function() { return this.findElement(this.by.id('cheese-row')); } },
+    cheeseText:   				{ get: function() { return this.findElement(this.by.id('cheese-text')); } },
+    cheeseQty:   					{ get: function() { return this.findElement(this.by.id('cheese-qty')); } },
+    cheeseQtyType:   					{ get: function() { return this.findElement(this.by.id('cheese-qtyType')); } },
 });
