9b72d6f0bb271db6f12ba4d691bb7895848875b6
Eric Wooley <ericwooley@gmail.com>
2014-06-21 23:43:49 -0700

    Remove console log

--- app/scripts/controllers/newlist.js
+++ app/scripts/controllers/newlist.js
@@ -11,9 +11,7 @@ angular.module('App.controllers')
 		var lists = storage.get(storageKeys.listsKey) || {};
 		// Create a new list from scripts/objects/list.js
 		$scope.list = new List({name: $scope.list.name, id:lists.length});
-		console.log('saving list', $scope.list);
 		lists[$scope.list.id] = $scope.list;
-		console.log('lists',lists);
 		storage.set(storageKeys.listsKey, lists);
 		$location.path('/mylists');
 	};
